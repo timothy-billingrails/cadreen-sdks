1d7e0673ae7237071390041e7a9a143884a3b3bf
timothy-billingrails <timothy@billingrails.com>
2026-06-22 06:28:06 +0100

    chore: reset Homebrew SHA256 to placeholders for v0.2.2 release workflow

--- Formula/cadreen.rb
+++ Formula/cadreen.rb
@@ -7,22 +7,22 @@ class Cadreen < Formula
   on_macos do
     on_arm do
       url "https://github.com/timothy-billingrails/cadreen-sdks/releases/download/cli-v#{VERSION}/cadreen_darwin_arm64"
-      sha256 "580b5ba99038cf607189b015499be74d3c50a2349c57394cbcd2c88e7b20a2df"
+      sha256 "PLACEHOLDER_ARM64_DARWIN_SHA256"
     end
     on_intel do
       url "https://github.com/timothy-billingrails/cadreen-sdks/releases/download/cli-v#{VERSION}/cadreen_darwin_amd64"
-      sha256 "f14e80fab4835e61a658a7219965353e9389ea9e0801900b8244ee9fb8c1ce49"
+      sha256 "PLACEHOLDER_AMD64_DARWIN_SHA256"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/timothy-billingrails/cadreen-sdks/releases/download/cli-v#{VERSION}/cadreen_linux_arm64"
-      sha256 "da1722f295a9a73d3a5435ce92790b58fe2f7d273dde666763554b66db27aa0c"
+      sha256 "PLACEHOLDER_ARM64_LINUX_SHA256"
     end
     on_intel do
       url "https://github.com/timothy-billingrails/cadreen-sdks/releases/download/cli-v#{VERSION}/cadreen_linux_amd64"
-      sha256 "ccd03c2430dbd85c5694ec994e5e44cb63d10124aa6cd48097a4917e96f95d39"
+      sha256 "PLACEHOLDER_AMD64_LINUX_SHA256"
     end
   end
 
